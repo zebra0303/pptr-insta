1bca274500a647b346872f8effc389581296bb43
seyoun.kim <zebra0303@gmail.com>
2020-12-02 20:45:12 +0900

    공통 로직은 함수로 빼라

--- index.js
+++ index.js
@@ -15,6 +15,29 @@ require('dotenv').config({path: `.env.${environment}`});
 const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 
+async function saveAPIData(page, url, filename) {
+  try {
+    await page.goto(url);
+    const apiData = await page.evaluate(() =>  {
+        return document.querySelector('body').innerText;
+    });
+    fs.writeFileSync(filename, apiData);
+  } catch (err) {
+    console.error(`사이트 정보 저장 에러!!! - ${url}`,  err);
+    return false;
+  }
+}
+
+function getJSONData(filename) {
+  try {
+    const fileContent = fs.readFileSync(filename, {encoding:'utf8', flag:'r'});
+    return JSON.parse(fileContent.toString());
+  } catch (err) {
+    console.error(`파일읽기 에러!!! - ${filename}`,  err);
+    return false;
+  }
+}
+
 (async () => {
   const opt = {
     headless: parseJSON(process.env.PPTR_HEADLESS),
@@ -25,29 +48,26 @@ const fs = require('fs');
   const page = await browser.newPage();
 
   // 인스타 그램에 로그인
-  await page.goto(process.env.INSTA_LOGIN_URL);
-  await page.waitForSelector('input[name="username"]');
-  await page.focus('input[name="username"]');
-  await page.keyboard.type(process.env.INSTA_ID);
-  await page.focus('input[name="password"]');
-  await page.keyboard.type(process.env.INSTA_PWD);
-  await page.click('button[type="submit"]');
-  await new Promise(r => setTimeout(r, 3000));
+  try {
+    await page.goto(process.env.INSTA_LOGIN_URL);
+    await page.waitForSelector('input[name="username"]');
+    await page.focus('input[name="username"]');
+    await page.keyboard.type(process.env.INSTA_ID);
+    await page.focus('input[name="password"]');
+    await page.keyboard.type(process.env.INSTA_PWD);
+    await page.click('button[type="submit"]');
+    await new Promise(r => setTimeout(r, 3000));
+  } catch (err) {
+    console.error('로그인 과정 에러!!!', err);
+    return false;
+  }
+
 
   // 인스타 그램 해시테그 페이지 방문해서 JSON코드 저장
-  await page.goto(process.env.INSTA_HASHTAG_URL);
-  const txtHashTagData = JSON.parse(await page.evaluate(() =>  {
-      return document.querySelector('body').innerText;
-  }));
+  await saveAPIData(page, process.env.INSTA_HASHTAG_URL, 'insta-hashtag.json');
 
   // 인스타그램 계정 페이지 방문해서 JSON코드 저장
-  await page.goto(process.env.INSTA_ACCOUNT_URL);
-  const txtAccountData = await page.evaluate(() =>  {
-      return document.querySelector('body').innerText;
-  });
-  fs.writeFileSync('insta-account.json', txtAccountData);
-
-
+  await saveAPIData(page, process.env.INSTA_ACCOUNT_URL, 'insta-account.json');
 
   const outputJSON = {
     cntHashTag : 0,
@@ -55,32 +75,34 @@ const fs = require('fs');
   }
 
   // 해시테그 JSON에서 필요한 정보 추출
-  const fileContentHashTag = fs.readFileSync('insta-hashtag.json', {encoding:'utf8', flag:'r'});
-  const jsonDataHashTag = JSON.parse(fileContentHashTag.toString());
-  outputJSON.cntHashTag = jsonDataHashTag.graphql.hashtag.edge_hashtag_to_media.count;
+  try {
+    const jsonDataHashTag = getJSONData('insta-hashtag.json');
+    outputJSON.cntHashTag = jsonDataHashTag.graphql.hashtag.edge_hashtag_to_media.count;
 
-  // 어카운트 JSON에서 필요한 정보 추출
-  const fileContentAccount = fs.readFileSync('insta-account.json', {encoding:'utf8', flag:'r'});
-  const jsonDataAccount = JSON.parse(fileContentAccount.toString());
-  jsonDataAccount.graphql.user.edge_owner_to_timeline_media.edges.map(edge => {
-    const node = edge.node;
-    const post = {
-      thumbnail: {
-        src: node.thumbnail_src,
-        // '150x150': node.thumbnail_resources[0].src,
-        // '240x240': node.thumbnail_resources[1].src,
-        '320x320': node.thumbnail_resources[2].src,
-        // '480x480': node.thumbnail_resources[3].src,
-        '640x640': node.thumbnail_resources[4].src
-      },
-      caption: node.edge_media_to_caption.edges[0].node.text,
-      link: `https://www.instagram.com/p/${node.shortcode}/`
-    };
+    // 어카운트 JSON에서 필요한 정보 추출
+    const jsonDataAccount = getJSONData('insta-account.json');
+    jsonDataAccount.graphql.user.edge_owner_to_timeline_media.edges.map(edge => {
+      const node = edge.node;
+      const post = {
+        thumbnail: {
+          src: node.thumbnail_src,
+          // '150x150': node.thumbnail_resources[0].src,
+          // '240x240': node.thumbnail_resources[1].src,
+          '320x320': node.thumbnail_resources[2].src,
+          // '480x480': node.thumbnail_resources[3].src,
+          '640x640': node.thumbnail_resources[4].src
+        },
+        caption: node.edge_media_to_caption.edges[0].node.text,
+        link: `https://www.instagram.com/p/${node.shortcode}/`
+      };
 
-    outputJSON.posts.push(post);
-  });
+      outputJSON.posts.push(post);
+    });
+  } catch (err) {
+    console.error('정보추출 에러!!!', err);
+  }
 
-  if (outputJSON.cntHashTag > 0 && outputJSON.posts.length > 0) {
+  if (outputJSON.cntHashTag >= 0 && outputJSON.posts.length >= 0) {
     fs.writeFileSync('insta-event.json', JSON.stringify(outputJSON));
   }
 
